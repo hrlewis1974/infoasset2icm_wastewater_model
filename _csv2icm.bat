34b53099236749ebcd19c47bc77ade32c43cc810
Hywel Lewis <hywel.r.lewis@gmail.com>
2024-07-22 13:11:36 +1200

    batch update

--- _csv2icm.bat
+++ _csv2icm.bat
@@ -6,6 +6,6 @@ set bit=64
 if %bit%==32 (set "path=C:\Program Files (x86)")
 if %bit%==64 (set "path=C:\Program Files")
 
-"%path%\Autodesk\InfoWorks ICM Ultimate 2024\ICMExchange.exe" "%~dp0%\ie_script.rb"
+"%path%\Autodesk\InfoWorks ICM Ultimate 2024\ICMExchange.exe" "%~dp0%\_csv2icm.rb"
 
 PAUSE

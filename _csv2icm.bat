f8fcaebac64f0f26300cf8538a5e233b945c480c
Hywel Lewis <hywel.r.lewis@gmail.com>
2024-07-22 13:11:04 +1200

    batch update

--- _csv2icm.bat
+++ _csv2icm.bat
@@ -1,7 +1,8 @@
 @echo off
-set version=2024
-set bit=64
 
+set version=2024
+
+set bit=64
 if %bit%==32 (set "path=C:\Program Files (x86)")
 if %bit%==64 (set "path=C:\Program Files")
 
--- _infoasset2csv.bat
+++ _infoasset2csv.bat
@@ -1,3 +1,12 @@
-"C:\Program Files (x86)\Innovyze Workgroup Client 2021.8\iexchange.exe" "C:\Users\HLewis\Downloads\ws_dma_statistics/backend\infoasset2csv.rb" /IA
+@echo off
+
+set version=2021.8
+if %version%==2021.8 (set "folder=Innovyze Workgroup Client 2021.8\iexchange.exe")
+
+set bit=32
+if %bit%==32 (set "path=C:\Program Files (x86)")
+if %bit%==64 (set "path=C:\Program Files")
+
+"%path%\%folder%" "%~dp0%\_infoasset2csv.rb" /IA
 
 PAUSE
